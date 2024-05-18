1bb544cfc5df61d8675eeb982566f7d3ddf940a6
Birzavit Alvarez <blaziken.bap@hotmail.com>
2024-05-18 17:55:40 -0500

    se aggaaron dependencias

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 
+import TextField from '@mui/material/TextField';
 
 function App() {
   return (
     <div className="App">
       <h1>hola</h1>
+      <TextField id="outlined-basic" label="Outlined" variant="outlined" />
     </div>
   );
 }
